107351e27cac70eaf9faac47128812da3b670f15
Jessica Bohn <bohn.16@wright.edu>
2017-02-05 00:04:03 -0600

    Fix Square onClick function
    The Square's onClick() function inside of its render had the syntax
    +> instead of =>, which is incorrect. Therefore it was corrected to
    the latter of the two.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() +> alert('click')}>
+      <button className="square" onClick={() => alert('click')}>
         {this.state.value}
       </button>
     );
