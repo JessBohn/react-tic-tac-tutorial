7c30f5aa60b3d74365472598ad20e2c90e6fe9f0
Jessica Bohn <bohn.16@wright.edu>
2017-02-05 00:41:55 -0600

    Convert Square render() button value back
    The Square render() had the button using this.state.value, which is
    now incorrect since state was added to Square's parent, Board.
    Therefore the value inside of the button tag was converted back to
    this.props.value.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square" onClick={() => alert('click')}>
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
