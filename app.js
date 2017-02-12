6f6bcbff098476cc57d6a0026dd78b7625cdd107
Jessica Bohn <bohn.16@wright.edu>
2017-02-11 23:21:02 -0600

    Add onClick handler to renderSquare()
    An onClick() was added to Square, but wasn't yet being passed down
    to as a prop to the Board. Therefore an onClick() was added inside
    of the renderSquare() inside of the Board class. So now Board is
    receiving value and onClick as props from Square.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]} onClick{() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
