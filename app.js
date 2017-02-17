abf139fc3f99d3b6b77edfcaae25df42ebc563dc
Jessica Bohn <bohn.16@wright.edu>
2017-02-16 23:31:22 -0600

    Add winner constant
    To declare the winner of the game a constant for winner had to be
    defined. The value of the constant is determined by the function
    calculateWinner() which uses the values of the squares on the board
    and their current values.

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ class Board extends React.Component {
     return <Square value={this.state.squares[i]} onClick{() => this.handleClick(i)} />;
   }
   render() {
+    const winner = calculateWinner(this.state.squares);
+
     const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
